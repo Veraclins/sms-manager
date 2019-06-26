e328f713d444ee1964f4399c4fb59fa021269e26
Agada Clinton Innocent <icstps4love@gmail.com>
2019-06-26 22:50:50 +0100

    Revert "create the sms manager"

--- src/helpers/index.js
+++
@@ -1,8 +0,0 @@
-export const sentenceCase = sentence =>
-  `${sentence[0].toUpperCase()}${sentence.substr(1)}`;
-
-export const humanize = text =>
-  text
-    .replace(/([A-Z])/g, ' $1')
-    .replace(/_/, ' ')
-    .toLowerCase();
